2cb7b08439c2c3056002573b9bade5526c82600b
Lewis Brown <lcb005@uark.edu>
2018-07-07 20:25:23 -0500

    threshold max / 3 to remove static ui elements

--- src/Main.py
+++ src/Main.py
@@ -6,21 +6,19 @@ import sys
 def main():
 
     streamParser = StreamParser()
-
-    streamParser.analyzeFootage("./output/1523129683.8167825.avi")
-
+    streamParser.analyzeFootage("./output/1530837385.423015.avi")
     sys.exit(0)
 
     streamManager = StreamManager()
 
     start = time.time()
 
-    streamManager.setUrl("https://www.twitch.tv/pvptwitch")
+    streamManager.setUrl("https://www.twitch.tv/linkzr")
     streamManager.setQuality("best")
     streamManager.setLogOutputPath("./output/" + str(start) + ".log")
     streamManager.setVideoOutputPath("./output/" + str(start) + ".avi")
-    streamManager.setScrapeEndConditon("iterations")
-    streamManager.setScrapeEndValue(100)
+    streamManager.setScrapeEndConditon("fileSize")
+    streamManager.setScrapeEndValue(10000000000)
 
     streamManager.startScrape(start)
 
--- src/StreamParser.py
+++ src/StreamParser.py
@@ -9,19 +9,76 @@ class StreamParser():
     def analyzeFootage(self, videoInputPath):
 
         capture = cv2.VideoCapture(videoInputPath)
+        sift = cv2.xfeatures2d.SIFT_create()
 
-        while(capture.isOpened()):
+        previous = {}
+        current = {}
 
-            ret, frame = capture.read()
+        previous["ret"], previous["frame"] = capture.read()
+        previous["gray"] = cv2.cvtColor(previous["frame"], cv2.COLOR_BGR2GRAY)
+        previous["blur"] = cv2.GaussianBlur(previous["gray"], (3, 3), 0)
+        previous["keypoints"] = sift.detect(previous["blur"], None)
+        previous["keypointsImage"] = cv2.drawKeypoints(previous["blur"], previous["keypoints"], previous["blur"])
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            blur = cv2.GaussianBlur(frame, (5, 5), 0)
-            edge = cv2.Canny(blur, 50, 150)
+        staticMap = np.zeros(previous["gray"].shape, dtype=np.uint8)
+        max = 1
 
-            cv2.imshow("frame", edge)
+        for z in range(0, 10):
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            current["ret"], current["frame"] = capture.read()
+            current["gray"] = cv2.cvtColor(current["frame"], cv2.COLOR_BGR2GRAY)
+            current["blur"] = cv2.GaussianBlur(current["gray"], (3, 3), 0)
+            current["keypoints"] = []
+            potentialKeypoints = sift.detect(current["blur"], None)
+
+            for potentialKeypoint in potentialKeypoints:
+                xa = int(potentialKeypoint.pt[0])
+                ya = int(potentialKeypoint.pt[1])
+
+                if staticMap[ya][xa] < max / 3:
+
+                    da = int(potentialKeypoint.size)
+                    aa = int(potentialKeypoint.angle)
+
+                    found = False
+
+                    for previousKeypoint in previous["keypoints"]:
+                        xb = int(previousKeypoint.pt[0])
+                        yb = int(previousKeypoint.pt[1])
+                        db = int(previousKeypoint.size)
+                        ab = int(previousKeypoint.angle)
+
+                        if xa == xb and ya == yb and da == db and aa == ab:
+                            found = True
+                            break
+
+                    if found:
+                        staticMap[ya][xa] += 1
+
+                        if staticMap[ya][xa] > max:
+                            max = staticMap[ya][xa]
+                    else:
+                        current["keypoints"].append(potentialKeypoint)
+
+            current["keypointsImage"] = cv2.drawKeypoints(current["blur"], current["keypoints"], current["blur"], flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+            print(z, "outof", 9, flush=True)
+
+            cv2.imwrite("output/reee/kps" + str(z) + ".jpg", current["keypointsImage"])
+
+            previous["ret"] = current["ret"]
+            previous["frame"] = current["frame"]
+            previous["gray"] = current["gray"]
+            previous["blur"] = current["blur"]
+            previous["keypoints"] = current["keypoints"]
+            previous["keypointsImage"] = current["keypointsImage"]
+
+        (rows, cols) = staticMap.shape
+
+        for y in range(0, rows):
+            for x in range(0, cols):
+                staticMap[y][x] = int(staticMap[y][x] * (255 / max))
+
+        cv2.imwrite("output/reee/static.jpg", staticMap)
 
         capture.release()
-        cv2.destroyAllWindows()
